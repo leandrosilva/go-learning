54d75c3ae4cca5a9f4108851774f2087a5af1a09
Leandro Silva <leandrodoze@gmail.com>
2012-05-11 02:35:12 -0300

    added a new app to test

---
+++ src/dislock/nuclear-test/nuclear.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"dislock/lock"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var uuid = flag.String("u", "nuclear_suff", "Lock UUID")
+	var client = flag.String("c", "homer_simpson", "Client ID")
+
+	flag.Parse()
+
+	var l = lock.New(*uuid, *client)
+
+	fmt.Println("Creating new lock: {UUID:", l.UUID, ", Client:", l.Client, ", Acquired:", l.Acquired, "}")
+	fmt.Println("Trying to acquire this lock")
+	l.Acquire()
+	fmt.Println("Could it acquire this lock:", l.Acquired)
+
+	fmt.Println("Type <RETURN> to QUIT...")
+	var key int
+	fmt.Scanf(">> %v", key)
+
+	fmt.Println("Releasing this lock...")
+	l.Release()
+	fmt.Println("This lock was released")
+}
